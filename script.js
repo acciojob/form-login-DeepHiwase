e644cb42db339e0f3694c158870970906d17bc43
Deep Hiwase <114879645+DeepHiwase@users.noreply.github.com>
2025-12-27 13:30:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function getFormvalue() {
     //Write your code here
 	// let firstName = document.getElementById("fName").trim();
 	// let lastName = document.getElementById("fName").trim();
-	let firstName = document.querySelector('input[name="fname"]').trim();
-	let lastName = document.querySelector('input[name="lname"]').trim();
+	let firstName = document.querySelector('input[name="fname"]').value().trim();
+	let lastName = document.querySelector('input[name="lname"]').value().trim();
 
 	alert(`${firstName} ${lastName}`);
 }
